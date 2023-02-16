92edfec577d1073c28641de05304407c08cb3ed4
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-02-16 10:39:01 +0000

    half way through example vignette also refactored maic() and maic.boot() for duplication

--- R/maic.R
+++ R/maic.R
@@ -1,60 +1,61 @@
 
 #' Estimate MAIC weights 
 #' 
-#' @param X.EM centered S=1 effect modifiers
+#' @param X.EM centred S=1 effect modifiers
 #' @return estimated weights
 #' 
 maic <- function(X.EM) {
   # objective function to be minimized for standard method of moments MAIC
-  Q <- function(alpha, X) {
-    sum(exp(X %*% alpha))
+  Q <- function(init, X) {
+    sum(exp(X %*% init))
   }
   
   X.EM <- as.matrix(X.EM)
+  
   N <- nrow(X.EM)
   K.EM <- ncol(X.EM)
-  alpha <- rep(1, K.EM) # arbitrary starting point for the optimizer
+  init <- rep(1, K.EM) # arbitrary starting point for the optimizer
   # objective function minimized using BFGS
-  Q.min <- optim(fn=Q, X=X.EM, par=alpha, method="BFGS")
-  hat.alpha <- Q.min$par # finite solution is the logistic regression parameters
-  log.hat.w <- rep(0, N)
+  Q.min <- optim(fn=Q, X=X.EM, par=init, method="BFGS")
+  
+  # finite solution is the logistic regression parameters
+  hat_alpha <- Q.min$par
+  log.hat_w <- rep(0, N)
+  
   for (k in seq_len(K.EM)) {
-    log.hat.w <- log.hat.w + hat.alpha[k]*X.EM[,k]
+    log.hat_w <- log.hat_w + hat_alpha[k]*X.EM[, k]
   }
   
-  exp(log.hat.w)
+  exp(log.hat_w)
 }
 
 
 #' MAIC bootstrap
 #' 
-#' @return fitted treatment coefficient is marginal effect for A vs. C
+#' @param data original data
+#' @param indices vector of indices which define the bootstrap sample
+#' @return fitted treatment coefficient is marginal effect for A vs C
 #' 
 maic.boot <- function(data, indices) {
-  dat <- data[indices, ]      # AC bootstrap sample
-  N <- nrow(dat)              # number of subjects in sample
-  x.EM <- dat[,c("X1","X2")]  # AC effect modifiers
+  dat <- data[indices, ]  # bootstrap sample
+  X.EM <- dat[,c("X1","X2")]  # AC effect modifiers
+
+  ##TODO: this seems odd. where is BC.ALD passed from?
+  ##      the call uses AC.IPD data
+  ##TODO: why is this centering used in maic.boot() and not maic()?
   # BC effect modifier means, assumed fixed
   theta <- BC.ALD[c("mean.X1", "mean.X2")]
-  K.EM <- ncol(x.EM)          # number of effect modifiers
-  # center the AC effect modifiers on the BC means
-  x.EM$X1 <- x.EM$X1 - theta$mean.X1
-  x.EM$X2 <- x.EM$X2 - theta$mean.X2
-  # MAIC weight estimation using method of moments
-  alpha <- rep(1,K.EM)        # arbitrary starting point for the optimizer
-  # objective function minimized using BFGS
-  Q.min <- optim(fn=Q, X.EM=as.matrix(x.EM), par=alpha, method="BFGS")
-  # finite solution is the logistic regression parameters
-  hat.alpha <- Q.min$par
-  log.hat.w <- rep(0, N)
-  for (k in seq_len(K.EM)) {
-    log.hat.w <- log.hat.w + hat.alpha[k]*x.EM[,k]
-  }
-  hat.w <- exp(log.hat.w)      # estimated weights
-  # fit weighted logistic regression model using glm
-  outcome.fit <- glm(y ~ trt, family="quasibinomial",
-                     weights=hat.w,
-                     data=dat)
+  # centre AC effect modifiers on BC means
+  X.EM$X1 <- X.EM$X1 - theta$mean.X1
+  X.EM$X2 <- X.EM$X2 - theta$mean.X2
+  
+  hat_w <- maic(X.EM)
+  
+  # fit weighted logistic regression model
+  outcome.fit <- glm(y ~ trt,
+                     family = "quasibinomial",
+                     weights = hat_w,
+                     data = dat)
   
   coef(outcome.fit)["trt"]
 }
