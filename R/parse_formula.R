2e940ab0bfa66b474be07a533b84b1de076e6a32
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2024-02-13 15:09:43 +0000

    finished parse_formula unit tests (for now)

--- R/parse_formula.R
+++ R/parse_formula.R
@@ -25,46 +25,61 @@ get_effect_modifiers <- function(formula) {
 #' @keywords internal
 #'
 get_treatment_name <- function(formula) {
+  
   formula <- as.formula(formula)
   term.labels <- attr(terms(formula), "term.labels")
   
   treat_nm <- term.labels[grepl(":", term.labels)]
   treat_nm <- gsub(":.+", "", treat_nm[1])
+  
+  if (is.na(treat_nm))
+    warning("Treatment name missing from formula.")
+  
   treat_nm
 }
 
 #' Get mean names
 #'
 #' @template args-ald
-#' @param var_names Variable names character vector
+#' @param keep_nms Variable names character vector
 #'
 #' @return Mean names vector
 #' @keywords internal
 #'
-get_mean_names <- function(ald, var_names) {
+get_mean_names <- function(ald, keep_nms) {
+  
   dat_names <- names(ald)
   # is_sd_name <- grepl(pattern = "\\.mean", dat_names)
   is_mean_name <- grepl(pattern = "mean\\.", dat_names)
-  is_var_name <- grepl(pattern = paste(var_names, collapse = "|"), dat_names)
+  is_var_name <- grepl(pattern = paste(keep_nms, collapse = "|"), dat_names)
+  keep_mean_nm <- is_mean_name & is_var_name
   
-  dat_names[is_mean_name & is_var_name]
+  if (all(!keep_mean_nm))
+    warning("No matching mean names found.")
+  
+  dat_names[keep_mean_nm]
 }
 
 #' Get standard deviation names
 #'
 #' @template args-ald
-#' @param var_names Variable names character vector
+#' @param keep_nms Variable names character vector
 #'
 #' @return Standard deviation names vector
 #' @keywords internal
 #'
-get_sd_names <- function(ald, var_names) {
+get_sd_names <- function(ald, keep_nms) {
+  
   dat_names <- names(ald)
   # is_sd_name <- grepl(pattern = "\\.sd", dat_names)
   is_sd_name <- grepl(pattern = "sd\\.", dat_names)
-  is_var_name <- grepl(pattern = paste(var_names, collapse = "|"), dat_names)
+  is_var_name <- grepl(pattern = paste(keep_nms, collapse = "|"), dat_names)
+  keep_sd_nm <- is_sd_name & is_var_name
   
-  dat_names[is_sd_name & is_var_name]
+  if (all(!keep_sd_nm))
+    warning("No matching sd names found.")
+  
+  dat_names[keep_sd_nm]
 }
 
 #' Get covariate names
--- tests/testthat/test-parse_formula.R
+++ tests/testthat/test-parse_formula.R
@@ -4,27 +4,38 @@
 
 test_that("test get_effect_modifiers", {
   expect_equal(get_effect_modifiers(y ~ x), character(0))
-  expect_equal(get_effect_modifiers(y ~ x + z), "z")
+  expect_equal(get_effect_modifiers(y ~ x + z), character(0))
+  expect_equal(get_effect_modifiers(y ~ x*z), "z") 
+  expect_equal(get_effect_modifiers(y ~ x + x*z), "z") 
+  expect_equal(get_effect_modifiers(y ~ x*z + c), "z") 
 })
 
 test_that("test get_treatment_name", {
-  expect_equal(get_treatment_name(y ~ x), "x")
-  expect_equal(get_treatment_name(y ~ x + z), "x")
-  expec
+  expect_warning(get_treatment_name(y ~ x), regexp = "Treatment name missing from formula.")
+  suppressWarnings(expect_equal(get_treatment_name(y ~ x), NA_character_))
+  expect_equal(get_treatment_name(y ~ x*z), "x")
 })
 
 test_that("get_mean_names", {
-  expect_equal(get_mean_names(y ~ x), "y")
-  expect_equal(get_mean_names(y ~ x + z), "y")
+  expect_equal(get_mean_names(data.frame("mean.x"=NA, "x"=NA), "x"), "mean.x")
+  expect_equal(get_mean_names(data.frame("mean.x"=NA), "x"), "mean.x")
+  expect_warning(get_mean_names(data.frame("x"=NA), "x"), regexp = "No matching mean names found.")
+  suppressWarnings(expect_equal(get_mean_names(data.frame("x"=NA), "x"), character(0)))
+  suppressWarnings(expect_equal(get_mean_names(data.frame("mean."=NA, "x"=NA), "x"), character(0)))
 })
 
 test_that("get_sd_names", {
-  expect_equal(get_sd_names(y ~ x), "y")
-  expect_equal(get_sd_names(y ~ x + z), "y")
+  expect_equal(get_sd_names(data.frame("sd.x"=NA, "x"=NA), "x"), "sd.x")
+  expect_equal(get_sd_names(data.frame("sd.x"=NA), "x"), "sd.x")
+  expect_warning(get_sd_names(data.frame("x"=NA), "x"), regexp = "No matching sd names found.")
+  suppressWarnings(expect_equal(get_sd_names(data.frame("x"=NA), "x"), character(0)))
+  suppressWarnings(expect_equal(get_sd_names(data.frame("sd."=NA, "x"=NA), "x"), character(0)))
 })
 
 test_that("get_covariate_names", {
-  expect_equal(get_covariate_names(y ~ x), character(0))
-  expect_equal(get_covariate_names(y ~ x + z), "z")
+  expect_equal(get_covariate_names(y ~ x), "x")
+  expect_equal(get_covariate_names(y ~ x + z), c("x","z"))
+  expect_equal(get_covariate_names(y ~ x*z), c("x","z"))
+  expect_equal(get_covariate_names(y ~ x*z + z), c("x","z"))
 })
 
