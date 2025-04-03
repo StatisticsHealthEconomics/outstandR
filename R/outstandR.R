6f9260d0d30cc6b30b9ac69ef66f424dbf674754
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-04-03 16:23:17 +0100

    stdReg2 example running. not sure if its the correct answer though

--- R/outstandR.R
+++ R/outstandR.R
@@ -47,6 +47,10 @@
 outstandR <- function(AC.IPD, BC.ALD, strategy, CI = 0.95, scale = NULL, ...) {
   
   if (CI <= 0 || CI >= 1) stop("CI argument must be between 0 and 1.")
+  
+  if (!any(scale %in% c("log_odds", "log_relative_risk", "risk_difference")))
+      stop("scale not in available list.")
+      
   ##TODO: as method instead?
   if (!inherits(strategy, "strategy"))
     stop("strategy argument must be a class strategy.")
