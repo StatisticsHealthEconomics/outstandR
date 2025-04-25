10067377263058ccc612a97fd89ecba781f599b4
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-04-25 17:19:00 +0100

    drafted some NEWS

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,11 @@
 # outstandR (development version) 0.009
 
-* Initial CRAN submission. TODO
 
-* 
+* Included correlation of ALD simulations as optional function argument (d48d0ab)
+* `print` method (983cb2f)
+* Absolute value returned, as well as contrasts (2f1fbd7)
+* Binary, continuous and count data outcomes allowed
+* Separate vignettes written for binary, continuous and count data
+* `pkgdown` site built and published on GitHub
+* Ability to select outcome scale from log-odds, risk difference, relative risk
+* Move covariate generation in to separate `simcovariate` package
--- R/validate_outstandr.R
+++ R/validate_outstandr.R
@@ -4,13 +4,17 @@ validate_outstandr <- function(AC.IPD, BC.ALD,
                                strategy,
                                CI, scale) {
   
-  if (CI <= 0 || CI >= 1) stop("CI argument must be between 0 and 1.")
+  if (CI <= 0 || CI >= 1) {
+    stop("CI argument must be between 0 and 1.")
+  }
   
-  if (!is.null(scale) && !any(scale %in% c("log_odds", "log_relative_risk", "risk_difference")))
+  if (!is.null(scale) && !any(scale %in% c("log_odds", "log_relative_risk", "risk_difference"))) {
     stop("scale not in available list.")
+  }
   
-  if (!inherits(strategy, "strategy"))
+  if (!inherits(strategy, "strategy")) {
     stop("strategy argument must be a class strategy.")
+  }
  
   return()  
 }
