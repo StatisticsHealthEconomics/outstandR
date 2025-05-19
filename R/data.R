2fb34cbe4cbd377e91cd72cfe70bae0fdcfc1e58
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-05-19 22:04:24 +0100

    change to r-universe

--- R/data.R
+++ R/data.R
@@ -5,44 +5,62 @@
 #' 
 #' @name AC_IPD
 #' @docType data
-#' @format A data list including the variables needed for the outstandR basic example. The variables are as follows:
+#' @format A data list including the variables needed for a outstandR basic example. The variables are as follows:
 #' \describe{
-#' \item{list("X1")}{Numeric covariate}
-#' \item{list("X2")}{Numeric covariate}
-#' \item{list("X3")}{Numeric covariate}
-#' \item{list("X4")}{Numeric covariate}
-#' \item{list("trt")}{Integer treatment identifier}
-#' \item{list("y")}{Integer binary outcome}
+#' \item{X1}{Numeric covariate}
+#' \item{X2}{Numeric covariate}
+#' \item{X3}{Numeric covariate}
+#' \item{X4}{Numeric covariate}
+#' \item{trt}{Integer treatment identifier}
+#' \item{y}{Integer binary outcome}
 #' }
 #' @references Remiro‐Azócar A, Heath A, Baio G (2022)
-#' 
+#' @source Simulated data
 #' @keywords datasets
 NULL
 
-#' Data set for aggregate level patient data
+#' Data set for aggregate level patient data in wide format
 #' 
 #' This data set contains summaries of simulated patient covariate and outcome values.
 #' 
 #' @name BC_ALD
 #' @docType data
-#' @format A data list including the variables needed for the outstandR basic example. The variables are as follows:
+#' @format A data list including the variables needed for a outstandR basic example. The variables are as follows:
 #' \describe{
-#' \item{list("mean.X1")}{Numeric value}
-#' \item{list("mean.X2")}{Numeric value}
-#' \item{list("mean.X3")}{Numeric value}
-#' \item{list("mean.X4")}{Numeric value}
-#' \item{list("sd.X1")}{Numeric standard deviation value}
-#' \item{list("sd.X2")}{Numeric standard deviation value}
-#' \item{list("sd.X3")}{Numeric standard deviation value}
-#' \item{list("sd.X4")}{Numeric standard deviation value}
-#' \item{list("y.B.sum")}{Integer number of patients with event on B treatment}
-#' \item{list("y.B.bar")}{Numeric proportion of patients on B treatment}
-#' \item{list("N.B")}{Integer Total number of patients on B treatment}
-#' \item{list("y.C.sum")}{Integer number of patients with event on C treatment}
-#' \item{list("y.C.bar")}{Numeric proportion of patients on C treatment}
-#' \item{list("N.C")}{Integer Total number of patients on C treatment}
+#' \item{mean.X1}{Numeric value}
+#' \item{mean.X2}{Numeric value}
+#' \item{mean.X3}{Numeric value}
+#' \item{mean.X4}{Numeric value}
+#' \item{sd.X1}{Numeric standard deviation value}
+#' \item{sd.X2}{Numeric standard deviation value}
+#' \item{sd.X3}{Numeric standard deviation value}
+#' \item{sd.X4}{Numeric standard deviation value}
+#' \item{y.B.sum}{Integer number of patients with event on B treatment}
+#' \item{y.B.bar}{Numeric proportion of patients on B treatment}
+#' \item{N.B}{Integer Total number of patients on B treatment}
+#' \item{y.C.sum}{Integer number of patients with event on C treatment}
+#' \item{y.C.bar}{Numeric proportion of patients on C treatment}
+#' \item{N.C}{Integer total number of patients on C treatment}
 #' }
 #' @references Remiro‐Azócar A, Heath A, Baio G (2022)
+#' @source Simulated data
+#' @keywords datasets
+NULL
+
+#' Data set for aggregate level patient data in long format
 #' 
+#' This data set contains summaries of simulated patient covariate and outcome values.
+#' 
+#' @name BC_ALD_long
+#' @docType data
+#'
+#' @format A data frame with 4 variables:
+#' \describe{
+#'   \item{statistic}{A character string}
+#'   \item{trt}{A factor}
+#'   \item{value}{A numeric value}
+#'   \item{variable}{A character string}
+#' }
+#' @source Simulated data
 #' @keywords datasets
 NULL
