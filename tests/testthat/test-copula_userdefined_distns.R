f379c247d53d219ea55dc555e9d7bc51f3ddca1c
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-07-18 11:31:55 +0100

    udpated tests for new gen_data()

--- tests/testthat/test-copula_userdefined_distns.R
+++ tests/testthat/test-copula_userdefined_distns.R
@@ -1,10 +1,11 @@
 #
 
+library(simcovariates)
 
 form <- y ~ PF_cont_1 + PF_cont_2 + trt*EM_cont_1 + trt*EM_cont_2
 
-###########################
-# generate continuous data
+#####################################
+# generate continuous covariate data
 
 N <- 200
 allocation <- 2 / 3      # active treatment vs. placebo allocation ratio (2:1)
@@ -20,37 +21,66 @@ sdX_EM <- c(0.4, 0.4)  # standard deviation of each EM covariate
 corX <- 0.2            # covariate correlation coefficient
 b_0 <- -0.6            # baseline intercept coefficient; fixed value
 
+# ipd
+
+covariate_defns_ipd <- list(
+  PF_cont_1 = list(type = continuous(mean = meanX_AC[1], sd = sdX[1]),
+                   role = "prognostic"),
+  PF_cont_2 = list(type = continuous(mean = meanX_AC[2], sd = sdX[2]),
+                   role = "prognostic"),
+  EM_cont_1 = list(type = continuous(mean = meanX_EM_AC[1], sd = sdX_EM[1]),
+                   role = "effect_modifier"),
+  EM_cont_2 = list(type = continuous(mean = meanX_EM_AC[2], sd = sdX_EM[2]),
+                   role = "effect_modifier")
+)
+
+b_prognostic <- c(PF_cont_1 = b_X, PF_cont_2 = b_X)
+
+b_effect_modifier <- c(EM_cont_1 = b_EM, EM_cont_2 = b_EM)
+
+num_normal_covs <- length(covariate_defns_ipd)
+cor_matrix <- matrix(corX, num_normal_covs, num_normal_covs)
+diag(cor_matrix) <- 1
+
+rownames(cor_matrix) <- c("PF_cont_1", "PF_cont_2", "EM_cont_1", "EM_cont_2")
+colnames(cor_matrix) <- c("PF_cont_1", "PF_cont_2", "EM_cont_1", "EM_cont_2")
+
 ipd_trial <- simcovariates::gen_data(
   N = N,
-  b_trt = b_trt,
-  b_X = b_X,
-  b_EM = b_EM,
   b_0 = b_0,
-  meanX = meanX_AC,
-  sdX = sdX,
-  meanX_EM = meanX_EM_AC,
-  sdX_EM = sdX_EM,
-  corX = corX,
-  allocation = allocation,
-  family = binomial("logit")
-)
+  b_trt = b_trt,
+  covariate_defns = covariate_defns_ipd,
+  b_prognostic = b_prognostic,
+  b_effect_modifier = b_effect_modifier,
+  cor_matrix = cor_matrix,
+  trt_assignment = list(prob_trt1 = allocation),
+  family = binomial("logit"))
 
 ipd_trial$trt <- factor(ipd_trial$trt, labels = c("C", "A"))
 
+# aggregate level data
+
+covariate_defns_ald <- list(
+  PF_cont_1 = list(type = continuous(mean = meanX_BC[1], sd = sdX[1]),
+                   role = "prognostic"),
+  PF_cont_2 = list(type = continuous(mean = meanX_BC[2], sd = sdX[2]),
+                   role = "prognostic"),
+  EM_cont_1 = list(type = continuous(mean = meanX_EM_BC[1], sd = sdX_EM[1]),
+                   role = "effect_modifier"),
+  EM_cont_2 = list(type = continuous(mean = meanX_EM_BC[2], sd = sdX_EM[2]),
+                   role = "effect_modifier")
+)
+
 BC.IPD <- simcovariates::gen_data(
   N = N,
-  b_trt = b_trt,
-  b_X = b_X,
-  b_EM = b_EM,
   b_0 = b_0,
-  meanX = meanX_BC,
-  sdX = sdX,
-  meanX_EM = meanX_EM_BC,
-  sdX_EM = sdX_EM,
-  corX = corX,
-  allocation = allocation,
-  family = binomial("logit")
-)
+  b_trt = b_trt,
+  covariate_defns = covariate_defns_ald,
+  b_prognostic = b_prognostic,
+  b_effect_modifier = b_effect_modifier,
+  cor_matrix = cor_matrix,
+  trt_assignment = list(prob_trt1 = allocation),
+  family = binomial("logit"))
 
 BC.IPD$trt <- factor(BC.IPD$trt, labels = c("C", "B"))
 
@@ -528,9 +558,9 @@ test_that("simulate_ALD_pseudo_pop directly", {
 
 test_that("simulate_ALD_pseudo_pop continuous via outstandR", {
   ##TODO:
-    # rho = rho_new,
-    # trt_var = "trt",
-    # N = 10000,
+  # rho = rho_new,
+  # trt_var = "trt",
+  # N = 10000,
   
   marginals_orig <- list(
     marginal_dists = c(EM_cont_1 = "norm",
@@ -558,8 +588,8 @@ test_that("simulate_ALD_pseudo_pop continuous via outstandR", {
 
 
 
-#######################
-# generate binary data
+#################################
+# generate binary covariate data
 
 form <- y ~ PF_bin_1 + PF_bin_2 + trt:EM_bin_1 + trt:EM_bin_2
 
@@ -575,33 +605,64 @@ propX_EM_BC <- c(0.6, 0.6)
 corX <- 0.2            # covariate correlation coefficient
 b_0 <- -0.6            # baseline intercept coefficient; fixed value
 
+covariate_defns_ipd <- list(
+  PF_bin_1 = list(type = binary(prob = propX_AC[1]),
+                  role = "prognostic"),
+  PF_bin_2 = list(type = binary(prob = propX_AC[2]),
+                  role = "prognostic"),
+  EM_bin_1 = list(type = binary(prob = propX_EM_AC[1]),
+                  role = "effect_modifier"),
+  EM_bin_2 = list(type = binary(prob = propX_EM_AC[2]),
+                  role = "effect_modifier")
+)
+
+b_prognostic <- c(PF_bin_1 = b_X, PF_bin_2 = b_X)
+
+b_effect_modifier <- c(EM_bin_1 = b_EM, EM_bin_2 = b_EM)
+
+num_normal_covs <- length(covariate_defns_ipd)
+cor_matrix <- matrix(corX, num_normal_covs, num_normal_covs)
+diag(cor_matrix) <- 1
+
+rownames(cor_matrix) <- c("PF_bin_1", "PF_bin_2", "EM_bin_1", "EM_bin_2")
+colnames(cor_matrix) <- c("PF_bin_1", "PF_bin_2", "EM_bin_1", "EM_bin_2")
+
 ipd_trial <- simcovariates::gen_data(
   N = N,
-  b_trt = b_trt,
-  b_X_bin = b_X,
-  b_EM_bin = b_EM,
-  prob_X_bin = propX_AC,
-  prob_EM_bin = propX_EM_AC,
   b_0 = b_0,
-  corX = corX,
-  allocation = allocation,
-  family = binomial("logit")
-)
+  b_trt = b_trt,
+  covariate_defns = covariate_defns_ipd,
+  b_prognostic = b_prognostic,
+  b_effect_modifier = b_effect_modifier,
+  cor_matrix = cor_matrix,
+  trt_assignment = list(prob_trt1 = allocation),
+  family = binomial("logit"))
 
 ipd_trial$trt <- factor(ipd_trial$trt, labels = c("C", "A"))
 
+# aggregate level data
+
+covariate_defns_ald <- list(
+  PF_bin_1 = list(type = binary(prob = propX_BC[1]),
+                  role = "prognostic"),
+  PF_bin_2 = list(type = binary(prob = propX_BC[2]),
+                  role = "prognostic"),
+  EM_bin_1 = list(type = binary(prob = propX_EM_BC[1]),
+                  role = "effect_modifier"),
+  EM_bin_2 = list(type = binary(prob = propX_EM_BC[2]),
+                  role = "effect_modifier")
+)
+
 BC.IPD <- simcovariates::gen_data(
   N = N,
-  b_trt = b_trt,
-  b_X_bin = b_X,
-  b_EM_bin = b_EM,
-  prob_X_bin = propX_BC,
-  prob_EM_bin = propX_EM_BC,
   b_0 = b_0,
-  corX = corX,
-  allocation = allocation,
-  family = binomial("logit")
-)
+  b_trt = b_trt,
+  covariate_defns = covariate_defns_ald,
+  b_prognostic = b_prognostic,
+  b_effect_modifier = b_effect_modifier,
+  cor_matrix = cor_matrix,
+  trt_assignment = list(prob_trt1 = allocation),
+  family = binomial("logit"))
 
 BC.IPD$trt <- factor(BC.IPD$trt, labels = c("C", "B"))
 
@@ -908,7 +969,7 @@ test_that("simulate directly with binary data without marginals", {
   #   expected = rho_new,
   #   tolerance = 0.1
   # )
-
+  
 })
 
 test_that("simulate directly with binary data with marginals", {
@@ -927,7 +988,7 @@ test_that("simulate directly with binary data with marginals", {
   )
   
   rho_new <- 0.6
-
+  
   # no rho ie taken from ipd
   result <- simulate_ALD_pseudo_pop(
     formula = form,
@@ -962,7 +1023,7 @@ test_that("simulate directly with binary data with marginals", {
     expected = marginals_orig$marginal_params$PF_bin_2$prob,
     tolerance = 0.01
   )
-
+  
   ##TODO: why are the binomial correlations wrong?
   
   # expect_equal(
@@ -1070,7 +1131,7 @@ test_that("simulate directly with binary data with marginals", {
   #   expected = rho_new,
   #   tolerance = 0.1
   # )
-
+  
   # via outstandR()
   
   expect_error(
