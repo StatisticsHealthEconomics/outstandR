7b7d55b78ef14b16f00c0a33d8a27744dc388301
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2025-07-22 14:48:44 +0100

    sorted package data to use in the joss paper

--- R/data.R
+++ R/data.R
@@ -1,66 +1,82 @@
 
-#' Data set for individual level patient data
+#' Individual-level patient data for binary outcome, continuous covariates
 #' 
 #' This data set contains simulated patient covariate and outcome values.
 #' 
-#' @name AC_IPD
+#' @name AC_IPD_binY_contX
 #' @docType data
-#' @format A data list including the variables needed for a outstandR basic example. The variables are as follows:
+#' @format ## `y ~ PF1 + PF2 + trt + trt:(EM1 + EM2)`
 #' \describe{
-#' \item{X1}{Numeric covariate}
-#' \item{X2}{Numeric covariate}
-#' \item{X3}{Numeric covariate}
-#' \item{X4}{Numeric covariate}
-#' \item{trt}{Integer treatment identifier}
-#' \item{y}{Integer binary outcome}
+#'    \item{id}{Numeric unique identifier}
+#'    \item{PF_cont_1}{Numeric prognostic factor continuous covariate}
+#'    \item{PF_cont_2}{Numeric prognostic factor continuous covariate}
+#'    \item{EM_cont_1}{Numeric effect modifier continuous covariate}
+#'    \item{EM_cont_2}{Numeric effect modifier continuous covariate}
+#'    \item{trt}{Factor treatment identifier. Levels A, C}
+#'    \item{y}{Integer binary outcome}
+#'    \item{true_eta}{Numeric linear predictor}
 #' }
 #' @references Remiro‐Azócar A, Heath A, Baio G (2022)
 #' @source Simulated data
 #' @keywords datasets
-NULL
+"AC_IPD_binY_contX"
 
-#' Data set for aggregate level patient data in wide format
+#' Aggregate level patient data for binary outcome, continuous covariates
 #' 
 #' This data set contains summaries of simulated patient covariate and outcome values.
 #' 
-#' @name BC_ALD
+#' @name BC_ALD_binY_contX
 #' @docType data
-#' @format A data list including the variables needed for a outstandR basic example. The variables are as follows:
+#' @format ## `y ~ PF1 + PF2 + trt + trt:(EM1 + EM2)`
 #' \describe{
-#' \item{mean.X1}{Numeric value}
-#' \item{mean.X2}{Numeric value}
-#' \item{mean.X3}{Numeric value}
-#' \item{mean.X4}{Numeric value}
-#' \item{sd.X1}{Numeric standard deviation value}
-#' \item{sd.X2}{Numeric standard deviation value}
-#' \item{sd.X3}{Numeric standard deviation value}
-#' \item{sd.X4}{Numeric standard deviation value}
-#' \item{y.B.sum}{Integer number of patients with event on B treatment}
-#' \item{y.B.bar}{Numeric proportion of patients on B treatment}
-#' \item{N.B}{Integer Total number of patients on B treatment}
-#' \item{y.C.sum}{Integer number of patients with event on C treatment}
-#' \item{y.C.bar}{Numeric proportion of patients on C treatment}
-#' \item{N.C}{Integer total number of patients on C treatment}
+#' \item{variable}{String covariate or outcome name. From EM_cont_1, EM_cont_2, PF_cont_1, PF_cont_2, y.}
+#' \item{statistic}{String summary statistic name. From mean, sd, sum, N}
+#' \item{value}{Numeric value}
+#' \item{trt}{Treatment (arm) name. From B, C}
 #' }
 #' @references Remiro‐Azócar A, Heath A, Baio G (2022)
 #' @source Simulated data
 #' @keywords datasets
-NULL
+"BC_ALD_binY_contX"
 
-#' Data set for aggregate level patient data in long format
+#' Individual-level patient data for continuous outcome, mixed covariates
 #' 
-#' This data set contains summaries of simulated patient covariate and outcome values.
+#' This data set contains simulated patient covariate and outcome values.
+#' Corresponds to ALD data set.
 #' 
-#' @name BC_ALD_long
+#' @name AC_IPD_contY_mixedX
 #' @docType data
-#'
-#' @format A data frame with 4 variables:
+#' @format ## `y ~ X1 + X3 + X4 + trt + trt:(X2 + X3 + X4)`
 #' \describe{
-#'   \item{statistic}{A character string}
-#'   \item{trt}{A factor}
-#'   \item{value}{A numeric value}
-#'   \item{variable}{A character string}
+#'    \item{id}{Numeric unique identifier}
+#'    \item{X1}{Numeric prognostic factor continuous covariate}
+#'    \item{X2}{Numeric prognostic factor and effect modifier binary covariate}
+#'    \item{X3}{Numeric prognostic factor and effect modifier continuous covariate}
+#'    \item{X4}{Numeric effect modifier binary covariate}
+#'    \item{trt}{Factor treatment identifier. Levels A, C}
+#'    \item{y}{Integer binary outcome}
+#'    \item{true_eta}{Numeric linear predictor}
 #' }
+#' @references Remiro‐Azócar A, Heath A, Baio G (2022)
 #' @source Simulated data
 #' @keywords datasets
-NULL
+"AC_IPD_contY_mixedX"
+
+#' Aggregate level patient data for continuous outcome, mixed covariates
+#' 
+#' This data set contains summaries of simulated patient covariate and outcome values.
+#' Corresponds to IPD data set.
+#' 
+#' @name BC_ALD_contY_mixedX
+#' @docType data
+#' @format ## `y ~ X1 + X3 + X4 + trt + trt:(X2 + X3 + X4)`
+#' \describe{
+#' \item{variable}{String covariate or outcome name. From X1, X2, X3, X4, y.}
+#' \item{statistic}{String summary statistic name. From mean, sd, sum, N}
+#' \item{value}{Numeric value}
+#' \item{trt}{Treatment (arm) name. From B, C}
+#' }
+#' @references Remiro‐Azócar A, Heath A, Baio G (2022)
+#' @source Simulated data
+#' @keywords datasets
+"BC_ALD_contY_mixedX"
